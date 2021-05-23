97d62efac6befa4b42f54b0cf398c6009d9a4b3a
nazonokouta0706 <g2027039@st.t-kougei.ac.jp>
2021-05-23 22:25:48 +0900

    ステートパターンに書き直しました。

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <thread>
 
+
+enum State
+{
+    STATE_CREDIT,
+    STATE_TITLE,
+    STATE_OPENING,
+    STATE_DEMO
+};
+
+
 int main()
 {
-repeat:
-    std::cout << "credit\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+    State state;
+    state = STATE_CREDIT;
 
-    std::cout << "title\n";
+    do
+    {
+        switch (state)
+        {
+        case STATE_CREDIT:
+            std::cout << "credit\n";
+            std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+        case STATE_TITLE:
+            std::cout << "title\n";
+            std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
 
-    std::cout << "opening\n";
+        case STATE_OPENING:
+            std::cout << "opening\n";
+            std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+        case STATE_DEMO:
+            std::cout << "demo\n";
+            std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
 
-    std::cout << "demo\n";
+        }
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+    } while (true);
     
-    goto repeat;
 }
 
